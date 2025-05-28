ceb1ff2f77a2093bc38c6969e3f712035679283c
sachinuke36 <sachinuke36@gmail.com>
2025-05-28 11:27:07 +0530

    responsiveness

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -66,52 +66,56 @@ const HomePage = () => {
   console.log(selectedProduct);
 
   return (
-    <div className="h-screen bg-gradient-to-br from-purple-600 to-indigo-700 flex items-center justify-center p-4">
-      <div className="h-7/8 bg-gray-200 w-11/12 shadow-indigo-900 shadow-xl">
-        {
-          selectedProduct == '' ? <><div className="flex justify-evenly items-center px-20">
-          <div className="shadow-lg rounded-2xl px-2 bg-white  shadow-purple-500 flex items-center justify-center gap-2">
-            <input
-              type="search"
-              placeholder="Search"
-              className="  bg-white rounded-l-2xl border-gray-500 px-2 w-[300px] focus:outline-none focus:border-purple-500 py-2"
-              onChange={(e) => setSearchTerm(e.target.value)}
-              name=""
-              id=""
-            />
-            <FaSearch
-              size={20}
-              color="purple"
-              className="font-extrabold mx-auto"
-            />
-          </div>
-          <Navbar active={active} setActive={setActive} />
-          <div className="shadow-lg p-2 rounded-2xl shadow-purple-500 bg-white">
-            <p className="font-bold text-amber-800 text-shadow-sm">
-              {user?.email.split("@")[0] || "User name"}
-            </p>
-          </div>
-        </div>
+    <div className="h-screen bg-gradient-to-br from-purple-600 to-indigo-700 flex items-center justify-center  md:p-4">
+      <div className="h-full md:h-7/8 bg-gray-200 border  w-full md:w-11/12 shadow-indigo-900 shadow-xl">
+        {selectedProduct == "" ? (
+          <>
+            <div className="flex flex-col gap-4 mt-6 md:mt-0 md:flex-row md:justify-evenly items-center  px-5 lg:px-20">
+              <div className="shadow-lg rounded-2xl px-2 bg-white  shadow-purple-500 flex items-center justify-center gap-2">
+                <input
+                  type="search"
+                  placeholder="Search"
+                  className="  bg-white rounded-l-2xl border-gray-500 px-2 w-full focus:outline-none focus:border-purple-500 py-2"
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  name=""
+                  id=""
+                />
+                <FaSearch
+                  size={20}
+                  color="purple"
+                  className="font-extrabold mx-auto"
+                />
+              </div>
+              <Navbar active={active} setActive={setActive} />
+              <div className="hidden lg:flex shadow-lg p-2 rounded-2xl shadow-purple-500 bg-white">
+                <p className="hidden md:block font-bold text-amber-800 text-shadow-sm">
+                  {user?.email.split("@")[0] || "User name"}
+                </p>
+              </div>
+            </div>
 
-        <div>
-          {active == "add-product" ? (
-            <AddProduct setData={setData} />
-          ) : (
-            <MyProducts
-              setModalOpen={setModalOpen}
-              setSelectedProduct={setSelectedProduct}
-              data={filteredData}
-            />
-          )}
-        </div></>:  <ProductModal
-        isOpen={modalOpen}
-        onClose={() => {
-          setModalOpen(false);
-          setSelectedProduct('')
-        }}
-        product={data?.find((item: any)=> item.id === selectedProduct )}
-      />
-        }
+            <div className="scroll-auto h-full max-h-[60%] overflow-y-scroll">
+              {active == "add-product" ? (
+                <AddProduct setData={setData} />
+              ) : (
+                <MyProducts
+                  setModalOpen={setModalOpen}
+                  setSelectedProduct={setSelectedProduct}
+                  data={filteredData}
+                />
+              )}
+            </div>
+          </>
+        ) : (
+          <ProductModal
+            isOpen={modalOpen}
+            onClose={() => {
+              setModalOpen(false);
+              setSelectedProduct("");
+            }}
+            product={data?.find((item: any) => item.id === selectedProduct)}
+          />
+        )}
       </div>
     </div>
   );
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar({active, setActive}: Props) {
     router.push('/login')
   }
   return (
-            <div className="w-4/10 h-15 rounded-3xl mx-auto my-5 shadow-purple-500 shadow-lg flex justify-center items-center gap-10 bg-white">
+            <div className="w-full border border-fuchsia-900  lg:w-4/10 h-15 rounded-3xl mx-auto my-5 shadow-purple-500 shadow-lg flex justify-center items-center gap-2 md:gap-10 bg-white">
                 <div onClick={()=>setActive('my-products')} className={`flex flex-col justify-center ${active==='my-products'?'border-0 border-b-2 pb-0.5 border-b-indigo-400' : ''}`  }>
                   <FaShoppingCart size={30} color="purple" className="font-extrabold mx-auto" />
                   <p className="text-xs text-indigo-800 font-bold">My products</p>
