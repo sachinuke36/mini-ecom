518b3ecfe115df7791e8f5cfc89f473867986391
sachinuke36 <sachinuke36@gmail.com>
2025-05-28 22:00:03 +0530

    added-frontend-url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -69,6 +69,7 @@ app.use(
   cors({
     origin: [
       "https://mini-ecom-git-main-sachinuke36s-projects.vercel.app",
+      "https://mini-ecom.vercel.app",
       "http://localhost:3000",
     ],
     credentials: true,
